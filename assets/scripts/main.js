2a37682587db01fa9e41fcf9db5fa39b3af0b5b1
Craig Fouts <foutscw@gmail.com>
2024-05-18 16:06:33 -0400

    Removed mail scripts

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -222,26 +222,3 @@
         location.href = '#' + id;
     }
 })(jQuery);
-
-const form = document.getElementById('contact-form');
-form.addEventListener('submit', function (event) {
-    const formValues = {
-        name: form.elements.name.value,
-        email: form.elements.email.value,
-        message: form.elements.message.value
-    };
-
-    const errors = validate(formValues, constraints);
-    
-    if (errors) {
-        event.preventDefault();
-        const errorMessage = Object
-            .values(errors)
-            .map(function (fieldValues) {
-                return fieldValues.join(', ')
-            })
-            .join('\n');
-
-        alert(errorMessage);
-    }
-}, false);
